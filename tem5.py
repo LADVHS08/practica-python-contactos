0d9c633645164b4496f44b5a075db8d39d3554a3
JhajairaChavez2006 <JhajairaChavez2006.com>
2026-06-04 22:13:59 -0500

    Ingresar datos y añadir registro

--- tem5.py
+++ tem5.py
@@ -1,1 +1,15 @@
 ARCHIVO="contactos.txt"
+
+#Ingresar datos por teclado.
+def ingresar_producto():
+    nombre = input("Nombre del producto: ")
+    stock = int(input("Stock: "))
+    precio = float(input("Precio: "))
+    return nombre, stock, precio
+
+#Añadir nuevos registros sin sobrescribir los anteriores.
+def guardar_producto():
+    nombre, stock, precio = ingresar_producto()
+
+    with open("inventario.txt", "a") as archivo:
+        archivo.write(f"{nombre},{stock},{precio}\n")

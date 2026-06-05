b592beb732168c84e6371d3b2e304ec84acbecd9
Mariela-P <N00477857@upn.pe>
2026-06-04 23:03:07 -0500

    Update tem5.py

--- tem5.py
+++ tem5.py
@@ -1,15 +1,16 @@
 ARCHIVO="contactos.txt"
 
 #Ingresar datos por teclado.
-def ingresar_producto():
-    nombre = input("Nombre del producto: ")
-    stock = int(input("Stock: "))
-    precio = float(input("Precio: "))
-    return nombre, stock, precio
-
-#Añadir nuevos registros sin sobrescribir los anteriores.
-def guardar_producto():
-    nombre, stock, precio = ingresar_producto()
-
-    with open("contactos.txt", "a") as archivo:
-        archivo.write(f"{nombre},{stock},{precio}\n")
+def ingresar_contacto():
+    id = input("Ingrese ID: ")
+    nombre = int(input("Nombre: "))
+    telefono = float(input("Teléfono: "))
+    return id, nombre, telefono
+    
+# Añadir nuevos registros sin sobrescribir los anteriores
+def guardar_contacto():
+    id_contacto, nombre, telefono = ingresar_contacto()
+    
+    with open(ARCHIVO, "a", encoding="utf-8") as archivo:
+        archivo.write(f"{id_contacto},{nombre},{telefono}\n")
+    print("Contacto guardado correctamente.")

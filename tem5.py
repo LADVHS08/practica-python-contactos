c0df3de0f5265fc34e9814622c0b1078b0273c7b
Mariela <N00477857@upn.pe>
2026-06-04 23:30:12 -0500

    Escribir los datos en un archivo texto

--- tem5.py
+++ tem5.py
@@ -1,1 +1,12 @@
 ARCHIVO="contactos.txt"
+
+# Escribir los datos en un archivo de texto
+def escribir_contacto():
+    id_contacto, nombre, telefono = ingresar_contacto()
+
+    with open(ARCHIVO, "w", encoding="utf-8") as archivo:
+        # Encabezado
+        archivo.write("\t".join(["ID", "Nombre", "Telefono"]) + "\n")
+        # Registro
+        archivo.write("\t".join([id_contacto, nombre, telefono]) + "\n")
+    print("Datos escritos correctamente.")

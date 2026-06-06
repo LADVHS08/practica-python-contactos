0c29229733e2622d92c5a9e9d826c37903f616da
Mariela-P <N00477857@upn.pe>
2026-06-05 22:07:14 -0500

    Update tem5.py

--- tem5.py
+++ tem5.py
@@ -15,7 +15,19 @@ def escribir_contacto():
         archivo.write("\t".join(["ID", "Nombre", "Telefono"]) + "\n")
         archivo.write("\t".join([id_contacto, nombre, telefono]) + "\n")
     print("Datos escritos correctamente.")
-    
+
+# Leer y mostrar el contenido del archivo línea por línea
+def mostrar_contactos():
+    try:
+        with open(ARCHIVO, "r", encoding="utf-8") as archivo:
+            print("\n--- CONTACTOS REGISTRADOS ---")
+
+            for linea in archivo:
+                print(linea.strip())
+
+    except FileNotFoundError:
+        print("No existe el archivo.")
+        
 # Añadir nuevos registros sin sobrescribir los anteriores
 def guardar_contacto():
     id_contacto, nombre, telefono = ingresar_contacto()
